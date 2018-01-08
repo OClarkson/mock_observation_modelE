1e9aa71936b0e6dae475f86d50940360ddd212f5
Yuka Fujii <yuka.fujii.ebihara@gmail.com>
2018-01-08 18:04:41 +0900

    Warning message appears when lowering resolution is not valid because of insufficient resolution in calculation.

--- transmission/resolution.py
+++ transmission/resolution.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import util_errors
 
 
 #=============================================================================
@@ -16,12 +16,18 @@ def lower_resolution( grid_wl, matrixW_sp, resolution ):
 
     # integral[ exp( - ( wl_i - wl_center) / sigma_center ) * sp ( wl_i ) * d wl_i ]
 
-    mesh_wl0, mesh_wl = np.meshgrid( grid_wl, grid_wl, indexing='ij' )
+    mesh_wl, mesh_wl0 = np.meshgrid( grid_wl, grid_wl, indexing='ij' )
     matrixW_convfunc  = gaussian( mesh_wl, mesh_wl0, resolution )
 
     matrixW_central = np.r_[ grid_wl[0], 0.5 * ( grid_wl[:-1] + grid_wl[1:] ), grid_wl[-1] ]
     matrixW_dwl     = np.diff( matrixW_central )
-    matrixW_sp_new    = np.dot( matrixW_convfunc, matrixW_sp * matrixW_dwl ) 
+    matrixW_sp_new  = np.dot( matrixW_convfunc, matrixW_sp * matrixW_dwl ) 
+
+    # FWHM > 0.5 * delta_wl ?
+    list_index = np.where( grid_wl[1:-1]/resolution < 0.5 * matrixW_dwl[1:-1] )[0]
+    if len( list_index ) > 0 :
+        util_errors.warning_longmsg( ['Due to the coarser grids of look-up tables than specified resolution, ',
+                                      'error in low-resolution spectrum is large beyond ~' + str( int( grid_wl[list_index[0]] ) ) + 'um.', ] )
 
     return matrixW_sp_new
 
