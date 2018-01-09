7f74dfcbdd681cb787d1b92a1663958602ecbdfd
Yuka Fujii <yuka.fujii.ebihara@gmail.com>
2018-01-09 17:01:11 +0900

    The edges of low-resolution spectrum is omitted due to large error.

--- transmission/resolution.py
+++ transmission/resolution.py
@@ -24,11 +24,16 @@ def lower_resolution( grid_wl, matrixW_sp, resolution ):
     matrixW_sp_new  = np.dot( matrixW_convfunc, matrixW_sp * matrixW_dwl ) 
 
     # FWHM > 0.5 * delta_wl ?
-    list_index = np.where( grid_wl[1:-1]/resolution < 0.5 * matrixW_dwl[1:-1] )[0]
+    grid_FWHM = grid_wl/resolution
+    list_index = np.where( grid_FWHM[1:-1] < 0.5 * matrixW_dwl[1:-1] )[0]
     if len( list_index ) > 0 :
         util_errors.warning_longmsg( ['Due to the coarser grids of look-up tables than specified resolution, ',
                                       'error in low-resolution spectrum is large beyond ~' + str( int( grid_wl[list_index[0]] ) ) + 'um.', ] )
 
-    return matrixW_sp_new
+    # 3 x FWMM
+    wl_min = np.where( grid_wl > grid_wl[0] + 3.*grid_FWHM[0] )[0][0]
+    wl_max = np.where( grid_wl < grid_wl[-1] - 3.*grid_FWHM[-1] )[0][-1]
+
+    return grid_wl[wl_min:wl_max+1], matrixW_sp_new[wl_min:wl_max+1]
 
 
