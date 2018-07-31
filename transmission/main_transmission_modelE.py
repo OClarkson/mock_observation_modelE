a785cc794cccd05dedd4c7d4ae88145815a7449a
Yuka Fujii <yuka.fujii@elsi.jp>
2018-07-31 21:44:16 +0900

    Error message when there are no molecular abosrption data.

--- transmission/main_transmission_modelE.py
+++ transmission/main_transmission_modelE.py
@@ -131,31 +131,41 @@ if __name__ == "__main__":
         # Adjust wavenumber range
         # if the wavenumber range of the lookup table is narrower, 
         # the wavenumber range of the lookup table is adopted
+        flag = 0
         for mol in list_mol :
+
             xsfile_tmp = s_xsFile_Tag + mol + ".npz" 
             if os.path.exists( xsfile_tmp ) : 
+                flag = 1
                 break
-        tmp = np.load( xsfile_tmp )
 
-        grid_wn_org = tmp['WN']
-        grid_T      = tmp['T']
-        grid_P      = tmp['P']*cgs.mbar_to_barye
+        if flag == 0 :
+            
+            util_errors.exit_msg( 'No molecular absorption data. If this is real, go to setup.py and set l_molecular_absorption=False and i_Wavenumber_num. ')
 
-        indx_min = read_xs.nearestindex_WN( grid_wn_org, f_Wavenumber_min )
-        indx_max = read_xs.nearestindex_WN( grid_wn_org, f_Wavenumber_max  )
-        if indx_min < indx_max :
-            grid_wn  = grid_wn_org[indx_min:indx_max+1]
         else :
-            util_errors.exit_msg( 'Specified wavenumber range is not valid---check the consistensy with the cross section table. ')
 
-        # get gridded data of cross section from HITRAN
-        dict_griddata_logXSofWNTP = read_xs.griddata_line( list_mol, s_xsFile_Tag, grid_wn, grid_T, grid_P, cnt_h2o_on=l_H2O_continuum )
+            tmp = np.load( xsfile_tmp )
 
-        if l_O3 and ( grid_wn[-1] > 1.e4 ) : # shortward of wavelength=1um
-            dict_griddata_logXSofWNTP = read_xs.griddata_add_UV( 'O3', 'data/SerdyuchenkoGorshelevVersionJuly2013.dat', grid_wn,  grid_T, grid_P, dict_griddata_logXSofWNTP )
+            grid_wn_org = tmp['WN']
+            grid_T      = tmp['T']
+            grid_P      = tmp['P']*cgs.mbar_to_barye
 
-        p_lookuptable_min = np.exp( np.min( dict_griddata_logXSofWNTP['coords'][:,1] ) )
-        p_lookuptable_max = np.exp( np.max( dict_griddata_logXSofWNTP['coords'][:,1] ) )
+            indx_min = read_xs.nearestindex_WN( grid_wn_org, f_Wavenumber_min )
+            indx_max = read_xs.nearestindex_WN( grid_wn_org, f_Wavenumber_max  )
+            if indx_min < indx_max :
+                grid_wn  = grid_wn_org[indx_min:indx_max+1]
+            else :
+                util_errors.exit_msg( 'Specified wavenumber range is not valid---check the consistensy with the cross section table. ')
+
+            # get gridded data of cross section from HITRAN
+            dict_griddata_logXSofWNTP = read_xs.griddata_line( list_mol, s_xsFile_Tag, grid_wn, grid_T, grid_P, cnt_h2o_on=l_H2O_continuum )
+
+            if l_O3 and ( grid_wn[-1] > 1.e4 ) : # shortward of wavelength=1um
+                dict_griddata_logXSofWNTP = read_xs.griddata_add_UV( 'O3', 'data/SerdyuchenkoGorshelevVersionJuly2013.dat', grid_wn,  grid_T, grid_P, dict_griddata_logXSofWNTP )
+
+            p_lookuptable_min = np.exp( np.min( dict_griddata_logXSofWNTP['coords'][:,1] ) )
+            p_lookuptable_max = np.exp( np.max( dict_griddata_logXSofWNTP['coords'][:,1] ) )
 
 
     else :
